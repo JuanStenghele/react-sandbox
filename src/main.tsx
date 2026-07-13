14805d4fd09099e5a2d893684260de21919b0202
Juan Stenghele <juanstenghele@gmail.com>
2026-07-13 02:02:51 +0000

    [ADD] - Auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from "react-router";
 import AppRoutes from "./routes";
 import { Provider } from 'jotai'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from "react-oidc-context";
+import { authOIDCConfig } from "./services/auth";
 
 const queryClient = new QueryClient({
   defaultOptions: { 
@@ -18,12 +20,14 @@ const root = document.getElementById("root")!;
 ReactDOM.createRoot(root).render(
   <StrictMode>
     <Provider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <CssBaseline />
-          <AppRoutes />
-        </BrowserRouter>
-      </QueryClientProvider>
+      <AuthProvider {...authOIDCConfig}>
+        <QueryClientProvider client={queryClient}>
+          <BrowserRouter>
+            <CssBaseline />
+            <AppRoutes />
+          </BrowserRouter>
+        </QueryClientProvider>
+      </AuthProvider>
     </Provider>
   </StrictMode>
 );
